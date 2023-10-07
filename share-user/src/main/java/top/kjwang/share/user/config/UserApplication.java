d9174a769193e27ad57af9c5bedec375824002d3
wkj <1729298243@qq.com>
2023-10-07 10:33:05 +0800

    修改启动日志，增加 /user-service 上下文，配置 logback 日志

--- share-user/src/main/java/top/kjwang/share/user/config/UserApplication.java
+++ share-user/src/main/java/top/kjwang/share/user/config/UserApplication.java
@@ -1,8 +1,10 @@
 package top.kjwang.share.user.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 /**
  * @author kjwang
@@ -12,8 +14,12 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("top.kjwang")
+@Slf4j
 public class UserApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
+		SpringApplication app = new SpringApplication(UserApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		log.info("启动成功！！！");
+		log.info("测试地址：http://127.0.0.1:{}{}/hello",env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
 	}
 }
